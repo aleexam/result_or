2fecd27638c515e867eabb97b14fc018c31a03a2
Ale <alexwaam@yahoo.com>
2025-05-04 17:50:28 +0300

    Error type fix

--- lib/src/errors.dart
+++ lib/src/errors.dart
@@ -8,14 +8,14 @@ abstract class BaseResultError {
   String get message => _message ?? runtimeType.toString();
 }
 
-/// Error class for Non-Fatal Errors
-/// You can extend this class to use your own errors class
+/// Exception class for Non-Fatal exceptions
+/// You can extend this class to use your own class
 class NonFatalResultError extends BaseResultError implements Exception {
   NonFatalResultError(super.message, [super.originalError]);
 }
 
 /// Error class for Fatal Errors
-/// Better use for handling fatal exceptions
+/// Better use for unexpected errors
 class FatalResultError extends BaseResultError implements Error {
   final StackTrace? _stack;
 
